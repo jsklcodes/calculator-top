b1f6ad183a661c0d160147c459222ba85d0aa39e
hellokallyne <hellookallyne@gmail.com>
2023-05-02 16:59:02 -0300

    Add variables for each part of the operation

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 const currentOperationDisplay = document.querySelector('.current-operation');
 const currentOperandDisplay = document.querySelector('.current-operand');
 const keyboard = document.querySelector('.keyboard');
+
+let previousOperand = '';
+let currentOperand = '';
+let operator = '';
