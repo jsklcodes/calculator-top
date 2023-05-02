f9ccadb8c5102953c19039125a17ff04f3563e2c
hellokallyne <hellookallyne@gmail.com>
2023-05-02 16:58:03 -0300

    Get element references in the DOM

---
+++ app.js
@@ -0,0 +1,3 @@
+const currentOperationDisplay = document.querySelector('.current-operation');
+const currentOperandDisplay = document.querySelector('.current-operand');
+const keyboard = document.querySelector('.keyboard');
